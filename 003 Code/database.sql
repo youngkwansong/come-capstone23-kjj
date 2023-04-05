a99693966315f77b2e6ec6d9367c154eb2209d85
송영관-모비젠 <iwarsong@naver.com>
2023-04-05 15:13:15 +0900

    Update database.sql

--- 003 Code/database.sql
+++ 003 Code/database.sql
@@ -2,7 +2,7 @@ create database keycloak CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
 GRANT ALL PRIVILEGES ON keycloak.* To 'keycloak'@'%' IDENTIFIED BY 'keycloak';
 show grants for 'keycloak'@'%';
 
-create database janbanzero CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
-GRANT ALL PRIVILEGES ON janbanzero.* To 'janbanzero'@'%' IDENTIFIED BY 'janbanzero!';
-show grants for 'janbanzero'@'%';
+create database zanbanzero CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
+GRANT ALL PRIVILEGES ON janbanzero.* To 'zanbanzero'@'%' IDENTIFIED BY 'zanbanzero!';
+show grants for 'zanbanzero'@'%';
 
